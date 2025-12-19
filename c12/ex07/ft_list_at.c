a2bddc2912d501b48d5c5af8c63e3d9efc91c134
Javi Barreiro <235066522+barraRRR@users.noreply.github.com>
2025-12-19 11:57:22 +0100

    submit: ex07 - norminette ok

---
+++ c12/ex07/ft_list_at.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_at.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarreir <jbarreir@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/19 11:49:35 by jbarreir          #+#    #+#             */
+/*   Updated: 2025/12/19 11:56:55 by jbarreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_at(t_list *begin_list, unsigned int nbr)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < nbr)
+	{
+		if (!begin_list)
+			return (NULL);
+		begin_list = begin_list->next;
+		i++;
+	}
+	return (begin_list);
+}
