03b388f456a76902e7943dd5a8abb92499b9c900
Javi Barreiro <235066522+barraRRR@users.noreply.github.com>
2025-12-19 14:47:15 +0100

    submit: ex11 - norminette ok

---
+++ c12/ex11/ft_list_find.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_find.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarreir <jbarreir@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/19 14:02:16 by jbarreir          #+#    #+#             */
+/*   Updated: 2025/12/19 14:46:46 by jbarreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
+{
+	if (!begin_list || !cmp)
+		return (NULL);
+	while (begin_list)
+	{
+		if (!cmp(begin_list->data, data_ref))
+			return (begin_list);
+		begin_list = begin_list->next;
+	}
+	return (NULL);
+}
