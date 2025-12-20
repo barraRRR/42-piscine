6258db21c5867c80a04a120c6207a748b315cb11
Javi Barreiro <235066522+barraRRR@users.noreply.github.com>
2025-12-20 22:28:25 +0100

    submit: ex01 - norminette ok

---
+++ c13/ex01/btree_apply_prefix.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_prefix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarreir <jbarreir@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/20 22:09:38 by jbarreir          #+#    #+#             */
+/*   Updated: 2025/12/20 22:27:55 by jbarreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	btree_apply_prefix(t_btree *root, void (*applyf)(void *))
+{
+	if (!root || !applyf)
+		return ;
+	applyf(root->item);
+	btree_apply_prefix(root->left, applyf);
+	btree_apply_prefix(root->right, applyf);
+}
