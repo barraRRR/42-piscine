5958bc4d1a0204845c0c6c46ca8e317b58b005da
Javi Barreiro <235066522+barraRRR@users.noreply.github.com>
2025-12-21 10:48:38 +0100

    submit: ex03 - norminette ok

---
+++ c13/ex03/btree_apply_suffix.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_suffix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarreir <jbarreir@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/21 10:45:24 by jbarreir          #+#    #+#             */
+/*   Updated: 2025/12/21 10:48:05 by jbarreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	btree_apply_suffix(t_btree *root, void (*applyf)(void *))
+{
+	if (!root || !applyf)
+		return ;
+	btree_apply_suffix(root->left, applyf);
+	btree_apply_suffix(root->right, applyf);
+	applyf(root->item);
+}
